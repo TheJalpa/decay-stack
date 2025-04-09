a6c9e7c27d9a3ca912b4dd5133160eb3bca95fa0
thejalpa <koihoshi@gmail.com>
2025-04-09 01:08:27 -0700

    fixing silly ref

--- moddownloader/modinstaller.py
+++ moddownloader/modinstaller.py
@@ -8,7 +8,7 @@ lines = f.read().splitlines()
 for line in lines:
     #print(line)
     try:
-        os.system('ln -s steamapps/workshop/content/221100/' + line + '/ /gamingshit/dayzserver/' + line)
+        os.system('ln -s steamapps/workshop/content/221100/' + line + '/' + homedir + '/' + line)
     except:
         print('ln -s failed, likely due to already existing')
     try:
@@ -17,5 +17,14 @@ for line in lines:
         print('Copying keys failed likely because of naming conventions, will try with keys instead of Keys')
     try:
         os.system('cd ' + homedir + ' && cp ' + line + '/keys/* keys/')
+    except:
+        print('Copying keys on second try failed, check file structure.')
+    try:
+        #because some people are annoying and deviate from naming conventions.
+        os.system('cd ' + homedir + ' && cp ' + line + '/key/* keys/')
+    except:
+        print('Copying keys on second try failed, check file structure.')
+    try:
+        os.system('cd ' + homedir + ' && cp ' + line + '/Key/* keys/')
     except:
         print('Copying keys on second try failed, check file structure.')
